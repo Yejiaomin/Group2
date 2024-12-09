1dfd4a14d10b76808f5354b0b346e6605f26982e
Ziting Wang <dwtintchev@outlook.com>
2024-12-09 15:00:59 -0800

    fixed scrollview on menu_list activity; deleted unused login functions

--- MobileAppGroup2_Frontend/app/src/main/java/com/example/foodMateFrontend/menu_activities/MenuListActivity.java
+++ MobileAppGroup2_Frontend/app/src/main/java/com/example/foodMateFrontend/menu_activities/MenuListActivity.java
@@ -3,10 +3,12 @@ package com.example.foodMateFrontend.menu_activities;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -65,6 +67,7 @@ public class MenuListActivity extends AppCompatActivity implements BottomNavigat
 
         setupExpandableMenus();
         fetchMenuItemsByCategory();
+
     }
 
 //    private void fetchMenus() {
